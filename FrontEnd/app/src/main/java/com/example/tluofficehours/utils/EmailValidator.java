0ecacb186586e52fa93cfc85e292005e4419098b
Văn Quý <137811354+vvquy04@users.noreply.github.com>
2025-06-17 12:30:18 +0700

    Revert "Vanquy"

--- FrontEnd/app/src/main/java/com/example/tluofficehours/utils/EmailValidator.java
+++
@@ -1,7 +0,0 @@
-package com.example.tluofficehours.utils;
-
-public class EmailValidator {
-    public static boolean isValidTLUEmail(String email) {
-        return email != null && email.endsWith("@e.tlu.edu.vn");
-    }
-}
