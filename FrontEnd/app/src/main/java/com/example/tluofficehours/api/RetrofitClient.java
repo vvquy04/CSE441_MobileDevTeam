209cd12cdc3e5ed3aaa936bc1a9ed2fd4e36e5dc
Van Quy <vivanquy20804@gmail.com>
2026-07-13 12:25:11 +0700

    config: switch from Ngrok to local IP for physical device testing

--- FrontEnd/app/src/main/java/com/example/tluofficehours/api/RetrofitClient.java
+++ FrontEnd/app/src/main/java/com/example/tluofficehours/api/RetrofitClient.java
@@ -14,7 +14,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://shimmer-itinerary-spilt.ngrok-free.dev/";
+    private static final String BASE_URL = "http://192.168.1.9:8000/";
     private static Retrofit retrofit = null;
 // HEAD
     private static Context context;
